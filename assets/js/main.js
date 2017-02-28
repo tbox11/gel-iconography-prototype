ad2c4957d49b10e10c65ec84c7b5057363911bdd
Simon Sinclair <simonsinclair@users.noreply.github.com>
2017-02-28 15:48:19 +0000

    Implement icon filtering based on search value

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+// Logic for Tom's prototype.
+// ==========================
+
+(function () {
+
+	var $searchField = $('#js-search-input');
+	var $icons = $('[data-icon-name]');
+
+	var App = {
+	    init: function() {
+	    	$searchField.on('input', App.filterIcons);
+	    },
+
+	    filterIcons: function(e) {
+	    	var tagQuery = $(this).val();
+
+	    	if (tagQuery === '') {
+	    		$icons.each(function (i, icon) {
+	    			$(icon).removeClass('gel-icon-demo--not-filtered');
+	    			$(icon).removeClass('gel-icon-demo--filtered');
+	    		});
+	    	} else {
+		    	$icons.each(function (i, icon) {
+		    		var tagName = $(icon).data().iconTag;
+
+	    			if (tagQuery === tagName) {
+	    				$(icon).addClass('gel-icon-demo--filtered');
+	    				$(icon).removeClass('gel-icon-demo--not-filtered');
+	    			} else {
+	    				$(icon).addClass('gel-icon-demo--not-filtered');
+	    				$(icon).removeClass('gel-icon-demo--filtered');
+	    			}
+		    	});
+	    	}
+	    }
+	};
+
+	// Start.
+	$(App.init);
+
+
+})();
